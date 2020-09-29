0b223f2d66984b1af18bc223acdc05fc32e90f6d
Dmitry Murzin <d.a.murzin@gmail.com>
2020-09-29 20:23:43 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0
+  let result = array[0]
+  array.slice(1).forEach(element => {
+    if (element < result) result = element
+  });
+  return result
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0
+  let result = array[0]
+  array.slice(1).forEach(element => {
+    if (element > result) result = element
+  });
+  return result
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) return 0
+  let result = 0
+  array.forEach(element => {
+    result += element
+  });
+  return result / array.length
 }
